4941838cc900fc093f3fd553fbc8f382a68df5fc
dfirpodcast <45311038+dfirpodcast@users.noreply.github.com>
2019-10-09 15:55:30 -0400

    Update file_hasher.py

--- file_hasher.py
+++ file_hasher.py
@@ -5,12 +5,10 @@ from datetime import datetime
 import sys
 
 #Creating argument for directory path:  file_hasher.py <directory>
-path = sys.argv[1]
+src_dir = sys.argv[1]
 
 #Timestamp to use in file name to mark when evidence was hashed
 timestamp = datetime.now().strftime("%d-%m-%Y_%I-%M-%S_%p")
-
-src_dir = sys.argv[1]
 
 def file_hash_hex(file_path, hash_func):
     with open(file_path, 'rb') as f:
@@ -21,6 +19,7 @@ def recursive_file_listing(base_dir):
         for filename in files:
             yield directory, filename, os.path.join(directory, filename)
 
+            
 if __name__ == "__main__":
 
     with open('Evidence_Hashes_' + timestamp + '.csv', 'w') as f:
